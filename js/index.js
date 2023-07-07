161bae02f06acd0165bb724338536359b93337de
ChpoCko <t2gaiu00@students.oamk.fi>
2023-07-07 23:55:06 +0300

    Cleaning

--- js/index.js
+++
@@ -1,9 +0,0 @@
-// const cards = document.querySelectorAll(".card");
-
-// cards.forEach(card => {
-//   // console.log(card);
-
-//   for (let i = 0; i < card.children.length; i++) {
-//     console.log(card.children[i]);
-//   }
-// });
