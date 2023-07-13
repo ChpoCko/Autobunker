0891796b2ae79f9a961a6ab0453fbf836b0d3669
ChpoCko <t2gaiu00@students.oamk.fi>
2023-07-13 18:44:19 +0300

    db.js __dirname fixed

--- server/db.js
+++ server/db.js
@@ -2,6 +2,12 @@ import pg from "pg";
 const { Pool } = pg;
 import fs from "fs";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
 dotenv.config();
 
 export const opendB = () => {
